52e8d9eeea28b11de2789c438e75db2693fc1667
Enrico Tolotto <etolotto@gmail.com>
2022-01-17 10:07:45 +0100

    update

--- scripts/install_vcpkg.bat
+++ scripts/install_vcpkg.bat
@@ -6,7 +6,7 @@ set VCPKG_ROOTDIR=%~dp0..\externals\vcpkg
 
 :: Ensure vcpkg repository is available (on developer machines)
 if not exist "%VCPKG_ROOTDIR%\.git" (
-    call git clone -depth 1 https://github.com/Microsoft/vcpkg.git %VCPKG_ROOTDIR%
+    call git clone --depth 1 https://github.com/Microsoft/vcpkg.git %VCPKG_ROOTDIR%
 )
 
 :: Build vcpkg
