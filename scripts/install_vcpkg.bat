7a48c032525e3e078d4d47fc219a58d016f3dd16
Enrico Tolotto <etolotto@gmail.com>
2022-01-17 10:05:00 +0100

    update

--- scripts/install_vcpkg.bat
+++ scripts/install_vcpkg.bat
@@ -6,13 +6,18 @@ set VCPKG_ROOTDIR=%~dp0..\externals\vcpkg
 
 :: Ensure vcpkg repository is available (on developer machines)
 if not exist "%VCPKG_ROOTDIR%\.git" (
-    call git clone https://github.com/Microsoft/vcpkg.git %VCPKG_ROOTDIR%
+    call git clone -depth 1 https://github.com/Microsoft/vcpkg.git %VCPKG_ROOTDIR%
 )
 
 :: Build vcpkg
 call "%VCPKG_ROOTDIR%\bootstrap-vcpkg.bat" -disableMetrics
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-:: Install required package(s)
-call "%VCPKG_ROOTDIR%\vcpkg.exe" install --triplet x64-windows gtest
-call "%VCPKG_ROOTDIR%\vcpkg.exe" install --triplet x86-windows gtest
+if ["%1"]==["all"] (
+    :: Install required package(s)
+    echo === Installing all the packages also for mingw
+    call "%VCPKG_ROOTDIR%\vcpkg.exe" install --triplet x64-windows gtest
+    call "%VCPKG_ROOTDIR%\vcpkg.exe" install --triplet x64-mingw-dynamic gtest
+) else (
+       call "%VCPKG_ROOTDIR%\vcpkg.exe" install --triplet x64-windows gtest
+)
