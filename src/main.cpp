69e3bbd0b08a58912496e51e8fe28d46e04558f5
Enrico Tolotto <etolotto@gmail.com>
2022-01-20 21:40:25 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,11 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) { return math::Formula::identity(0); }
+int main(int argc, char *argv[]) {
+  if (math::Formula::identity(0) == 0) {
+    std::cout << "Correct result\n";
+  } else {
+    std::cout << "False";
+  }
+  return 0;
+}
