4904a9d9c9d2e7790a059b7923d043f19d122dd0
Enrico Tolotto <etolotto@gmail.com>
2022-02-26 21:04:59 +0100

    fix test used for address sanitizer check

--- tst/formula-tests.cpp
+++ tst/formula-tests.cpp
@@ -2,10 +2,6 @@
 #include "tests_include.hpp"
 
 TEST(blaTest, test1) {
-  int *hello = new int[1000];
-  delete[] hello;
-  hello[0] = 5;
-  EXPECT_EQ(hello[0], 5);
   math::Formula formula;
   EXPECT_EQ(formula.identity(0), 0);
   EXPECT_EQ(formula.identity(10), 10);
